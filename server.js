47412cfa38163b2a9a44a5b666e8af714d2c206e
Liliana-Hernandez-Velasco <122941293+Liliana-Hernandez-Velasco@users.noreply.github.com>
2023-11-16 00:18:37 +0000

    Push 1

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-// index.js
+// server.js
 // where your node app starts
 
 // init project
 var express = require('express');
 var app = express();
-
+var convert = require('./convert.js');
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -18,12 +18,37 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+//current
+app.get("/api",(req,res)=> {res.json(convert.current());})
+//
+
+//input
+app.get("/api/:date", (req, res) => {
+  let dateString = req.params.date;
+
+  if (!isNaN(Date.parse(dateString))) {
+    let dateObject = new Date(dateString);
+    res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+  } else if (/\d{5,}/.test(dateString)) {
+      let dateInt = parseInt(dateString);
+      res.json({ unix: dateInt, utc: new Date(dateInt).toUTCString() });
+  } else {
+    res.json({ error: "Invalid Date" });
+  }
+
+});
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
 
 
 // listen for requests :)
